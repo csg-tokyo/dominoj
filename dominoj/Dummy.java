d968e807c0d3e44a914b1d18c6c607547ce06915
yungyu <zhuang@csg.ci.i.u-tokyo.ac.jp>
2014-06-17 19:45:27 +0900

    add license.
    adopt the modified BSD License (3-clause).

--- dominoj/Dummy.java
+++ dominoj/Dummy.java
@@ -1,3 +1,13 @@
+/*
+ * DominoJ is an extension to Java for supporting method slots,
+ * and this project is an implementation of its compiler built on top of JastAddJ.
+ * 
+ * Copyright (c) 2011-, YungYu Zhuang. All rights reserved.
+ *
+ * The contents of this file are licensed under the modified BSD License
+ * (please see the LICENSE file for the full text).
+ */
+
 package dominoj;
 
 public @interface Dummy {
